2a34663814b34ed7630646402401a18999039f38
Zoz-trk <ozioztourk@gmail.com>
2023-08-28 11:09:45 -0400

    General Configuration Implementations

--- chrome_extension/webpack.config.js
+++ chrome_extension/webpack.config.js
@@ -1,22 +1,68 @@
-const path = require('path');
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+  entry: {
+    index: "./src/index.js",
   },
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.[jt]s?$/,
+        loader: "esbuild-loader",
+        options: {
+          target: "es2015",
+          loader: "jsx",
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "manifest.json", to: "../manifest.json" }],
+    }),
+    ...getHtmlPlugins(["index"]),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js",
+  },
 };
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HTMLPlugin({
+        title: "Coin Tracker",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
